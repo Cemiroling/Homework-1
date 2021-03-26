a1784dd1ab12dad047ce8de2a48b71d1d38185d7
Cemiroling <45895475+Cemiroling@users.noreply.github.com>
2021-03-27 00:29:20 +0300

    Debug logging instead info

--- SecondModule/src/main/java/BinarySearch.java
+++ SecondModule/src/main/java/BinarySearch.java
@@ -7,22 +7,22 @@ public class BinarySearch {
 
     public static int Search(int[] ms, int startIndex, int endIndex, int value) {
         if (endIndex >= 1) {
-            logger.info("Index range: [" + startIndex + "-" + endIndex + "]");
+            logger.debug("Index range: [" + startIndex + "-" + endIndex + "]");
             int middle = startIndex + (endIndex - startIndex) / 2;
             if (value == ms[middle]) {
-                logger.info("Element found. Index - :" + middle);
+                logger.debug("Element found. Index - :" + middle);
                 return middle;
             }
             if (startIndex == endIndex){
-                logger.info("Element not found");
+                logger.debug("Element not found");
                 return -1;
             }
             if (value < ms[middle]) {
-                logger.info("Value is lower: " + value + "<" + ms[middle]);
+                logger.debug("Value is lower: " + value + "<" + ms[middle]);
                 return Search(ms, startIndex, middle - 1, value);
             }
             if (value > ms[middle]) {
-                logger.info("Value is higher: " + value + ">" + ms[middle]);
+                logger.debug("Value is higher: " + value + ">" + ms[middle]);
                 return Search(ms, middle + 1, endIndex, value);
             }
         }
